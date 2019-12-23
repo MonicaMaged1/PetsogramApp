f072d2eb541cb8501cbe2ef5a894647e604451d6
MarinaGamal <32170558+MarinaGamal@users.noreply.github.com>
2019-12-23 21:38:40 +0200

    Created design for add post fragment by either opening camera or gallery

--- app/src/main/java/com/example/group/petsogramapp/add/AddFragment.java
+++ app/src/main/java/com/example/group/petsogramapp/add/AddFragment.java
@@ -1,40 +1,131 @@
 package com.example.group.petsogramapp.add;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.group.petsogramapp.R;
+import com.example.group.petsogramapp.ui.home.HomeFragment;
+import com.example.group.petsogramapp.ui.profile.ProfileFragment;
+import com.example.group.petsogramapp.ui.profile.ProfileViewModel;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.Inflater;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class AddFragment extends Fragment {
 
     private AddViewModel mViewModel;
-
+    private ImageView selectedFromGallery;
+    View Root;
     public static AddFragment newInstance() {
         return new AddFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        Root = inflater.inflate(R.layout.fragment_add, container, false);
+
         return inflater.inflate(R.layout.fragment_add, container, false);
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(AddViewModel.class);
-        // TODO: Use the ViewModel
+        selectedFromGallery = (ImageView) getActivity().findViewById(R.id.selectedFromGallery);
+        selectImage(Root.getContext());
     }
 
 
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your image");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                        if (bitmap != null) {
+                            selectedFromGallery.setImageBitmap(bitmap);
+                        }
+                    }
 
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        if (data != null) {
+                            Uri contentURI = data.getData();
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), contentURI);
+                                selectedFromGallery.setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+
+                            }
+                        }
+                    }
+                    break;
+//                case 2:
+//                    HomeFragment newGamefragment = new HomeFragment();
+//                    FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                    fragmentTransaction.replace(R.id.navigation_add, newGamefragment);
+//                    fragmentTransaction.addToBackStack(null);
+//                    fragmentTransaction.commit();
+            }
+
+        }
+    }
 }
